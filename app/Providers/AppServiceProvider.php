ddd1633e244a1c2b3b888a77f5587e5199d94c2c
tembo-1 <tembo535@gmail.com>
2025-06-02 04:14:58 +0300

    test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Telegram\Bot\Laravel\Facades\Telegram;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Telegram::deleteMessage([
+            'help',
+            'start'
+        ]);
     }
 }
